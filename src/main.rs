e53e60d6db0a178822ca7ed132d270075cd2e3a9
iranika <7370486+iranika@users.noreply.github.com>
2019-01-29 16:23:56 +0900

    fix: rustc 1.33.0-nightlyではpanic_handlerに戻っていたため、修正

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,18 @@
 #![no_std]
 #![no_main]
-#![feature(panic_implementation)]
 
+//extern crate core;
 use core::panic::PanicInfo;
 
 /// This function is called on panic.
-#[panic_implementation]
+#[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-#[no_mangle]
-pub extern "C" fn main() -> ! {
+#[no_mangle] // don't mangle the name of this function
+pub extern "C" fn _start() -> ! {
+    // this function is the entry point, since the linker looks for a function
+    // named `_start` by default
     loop {}
 }
