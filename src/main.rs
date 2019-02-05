964cb73ce49cf10f92f56bf7e27563c4a2d4811d
iranika <7370486+iranika@users.noreply.github.com>
2019-02-05 21:07:36 +0900

    add: println macro. printing panic messages.

--- src/main.rs
+++ src/main.rs
@@ -9,14 +9,13 @@ mod vga_buffer;
 /// This function is called on panic.
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
+    println!("{}", _info);
     loop {}
 }
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    use core::fmt::Write;
-    vga_buffer::WRITER.lock().write_str("Hello again").unwrap();
-    write!(vga_buffer::WRITER.lock(), ", some numbers: {} {}", 42, 1.337).unwrap();
-
+    println!("Hello World{}", "!");
+    panic!("Some panic message");
     loop {}
 }
